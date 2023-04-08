7a1b5afc1a3a82dbfbad3229cd08347eedde5d49
Stuart Kennedy <rav2040@gmail.com>
2023-04-09 11:31:11 +1200

    Use regex for path split

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function main() {
     try {
         const client = create();
 
-        const paths = getInput("path").split("\n").map((str) => str.trim()).filter(Boolean);
+        const paths = getInput("path").split(/\s+(?=([^"]*"[^"]*")*[^"]*$)/g).map((str) => str.trim()).filter(Boolean);
         const uniquePaths = Array.from(new Set(paths));
 
         await Promise.all(uniquePaths.map(async (path) => {
