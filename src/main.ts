fb55c9031ccb300ed3c9ac13df1d19e77d824221
Stuart Kennedy <rav2040@gmail.com>
2023-04-08 16:04:30 +1200

    URI encode artifact name

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function main() {
         const uniquePaths = Array.from(new Set(paths));
 
         await Promise.all(uniquePaths.map((path) => {
-            return client.uploadArtifact(path, [path], process.cwd(), { continueOnError: false })
+            const name = encodeURIComponent(path);
+            return client.uploadArtifact(name, [path], process.cwd(), { continueOnError: false })
         }))
     } catch (err) {
         if (err instanceof Error) setFailed(err);
