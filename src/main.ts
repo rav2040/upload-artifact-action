09662533122f66b206a56866fda500aa1d829b36
Stuart Kennedy <rav2040@gmail.com>
2023-04-09 08:22:09 +1200

    Support uploading directory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createHash } from "crypto";
+import { walk } from '@nodelib/fs.walk';
 import { getInput, setFailed } from "@actions/core";
 import { create } from "@actions/artifact";
 
@@ -9,13 +10,26 @@ async function main() {
         const paths = getInput("path").split("\n").map((str) => str.trim()).filter(Boolean);
         const uniquePaths = Array.from(new Set(paths));
 
-        await Promise.all(uniquePaths.map((path) => {
+        await Promise.all(uniquePaths.map(async (path) => {
             const name = createHash("sha256").update(path).digest("hex");
-            return client.uploadArtifact(name, [path], ".", { continueOnError: false })
+            const paths = await getFileList(path);
+            return client.uploadArtifact(name, paths, ".", { continueOnError: false })
         }))
     } catch (err) {
         if (err instanceof Error) setFailed(err);
     }
+}
+
+function getFileList(path: string) {
+    return new Promise<string[]>((resolve, reject) => {
+        walk(path, { entryFilter: ({ dirent }) => !dirent.isDirectory() }, (err, entries) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(entries.map((entry) => entry.path));
+        });
+    });
 }
 
 main();
