f4301f057961da7249c2cbea387392de9499d62f
Stuart Kennedy <rav2040@gmail.com>
2023-04-11 20:52:34 +1200

    Testing

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,16 @@ async function main() {
     try {
         const paths = getMultilineInput("path", { required: true });
 
+        console.log("paths:", paths)
+
         await Promise.all(paths.map(async (path) => {
             const name = createHash("sha256").update(path).digest("hex");
 
             const globber = await createGlobClient(path);
             const paths = await globber.glob();
+
+            console.log("paths:", paths)
+
 
             return artifactClient.uploadArtifact(name, paths, ".", { continueOnError: false })
         }))
